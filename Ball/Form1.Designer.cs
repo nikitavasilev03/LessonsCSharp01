dca608b7a32736a83096cd226b9b986bb6167736
Никита Васильев <nikitavasilev2303@gmail.com>
2020-03-01 15:59:16 +0300

    add project Ball

---
+++ Ball/Form1.Designer.cs
@@ -0,0 +1,401 @@
+﻿namespace Ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(973, 625);
+            this.panel1.TabIndex = 0;
+            this.panel1.Resize += new System.EventHandler(this.panel1_Resize);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(8, 138);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(208, 32);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groupBox1.Location = new System.Drawing.Point(743, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(230, 625);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Параметры";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "минуты:секунды:милисекунды";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Время:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(58, 191);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(158, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Пикселей в метре:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(130, 29);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(72, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(130, 60);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(72, 20);
+            this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Кадров в секунду:";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(8, 100);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(208, 32);
+            this.btnStop.TabIndex = 10;
+            this.btnStop.Text = "Стоп";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnStop);
+            this.groupBox2.Controls.Add(this.numericUpDown2);
+            this.groupBox2.Controls.Add(this.btnStart);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.numericUpDown1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox2.Location = new System.Drawing.Point(3, 397);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(224, 225);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Параметры отрисовки модели";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox4);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.textBox7);
+            this.groupBox3.Controls.Add(this.label10);
+            this.groupBox3.Controls.Add(this.textBox6);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.textBox5);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox3.Location = new System.Drawing.Point(3, 16);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(224, 205);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Мяч";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(122, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Начальная высота (м):";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(150, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(65, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(150, 81);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(65, 20);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "-9,8";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "g (м/c^2):";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(150, 107);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(65, 20);
+            this.textBox5.TabIndex = 7;
+            this.textBox5.Text = "0,8";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(8, 110);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Упругость:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(150, 55);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(65, 20);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.Text = "0";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(8, 58);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(143, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Начальная скорость (м/c):";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(150, 177);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(65, 20);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(8, 180);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Скорость (м/c):";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(150, 151);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(65, 20);
+            this.textBox7.TabIndex = 11;
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(8, 154);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(65, 13);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Высота (м):";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 625);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Ball/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ball
+{
+    public partial class Form1 : Form
+    {
+        //
+        int mInPix = 20;
+        int xcenter;
+        int ycenter;
+        int miliseconds;
+        int frameInMiliseconds;
+        int allTime;
+
+        //Параметры меча
+        //Начальная скорость
+        double nSpeed = 0;
+        //Начальная высота
+        double nH = 0;
+        //Коэффициэнт упругости
+        double upr = 1;
+        //g - ускорение свободного падения
+        double g = -9.8;
+        //Нахождение меча по оси X
+        int ball_x = 400;
+        //
+        int timeVec = 0;
+
+        private BufferedGraphics canvas;
+        private Graphics graphics;
+        private BufferedGraphicsContext context;
+
+        Pen pen = new Pen(Color.Black);
+        SolidBrush brush = new SolidBrush(Color.Black);
+
+        //Конструктор формы
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Оброботчик события загрузки формы
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Объект для рисования на панели
+            graphics = panel1.CreateGraphics();
+            context = BufferedGraphicsManager.Current;
+            canvas = context.Allocate(graphics, panel1.DisplayRectangle);
+
+            //Точка (0, 0) на графике
+            xcenter = 30;
+            ycenter = panel1.Height;
+
+
+        }
+
+        //Метод отрисовки графика
+        private void DrawOXY()
+        {
+            pen.Width = 10;
+            canvas.Graphics.DrawLine(pen, 0, ycenter, panel1.Width, ycenter);
+            pen.Width = 5;
+            canvas.Graphics.DrawLine(pen, xcenter, 0, xcenter, ycenter);
+
+            pen.Width = 5;
+            int p = 1;
+            int t = xcenter;
+            while (t <= panel1.Width)
+            {
+                t += mInPix;
+                canvas.Graphics.DrawLine(pen, t, ycenter - 8, t, ycenter);
+            }
+            p = 1;
+            t = ycenter;
+            while (t >= 0)
+            {
+                t -= mInPix;
+                canvas.Graphics.DrawLine(pen, xcenter, t, xcenter + 6, t);
+                if (p % 5 == 0)
+                {
+                    canvas.Graphics.DrawString(p.ToString(), this.Font, brush, xcenter - 17, t - 6);
+                }
+                p++;
+            }
+        }
+
+        //Оброботчик изменения размера панели
+        private void panel1_Resize(object sender, EventArgs e)
+        {
+            canvas.Graphics.Clear(Color.White);
+
+            graphics = panel1.CreateGraphics();
+            context = BufferedGraphicsManager.Current;
+            canvas = context.Allocate(graphics, panel1.DisplayRectangle);
+
+            xcenter = 30;
+            ycenter = panel1.Height;
+
+            //DrawOXY();
+            //DrawBall(GetPoint(ball_x, (int)nH));
+        }
+
+        //Нажатие на кнопку Старт
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Пробуем взять данные с формы
+            try
+            {
+                nH = double.Parse(textBox2.Text);
+                nSpeed = double.Parse(textBox6.Text);
+                upr = double.Parse(textBox5.Text);
+                g = double.Parse(textBox3.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Некоректные значения!", "Ошибка!");
+                return;
+            }
+
+            //Определяем писелей в метре
+            mInPix = (int)numericUpDown1.Value;
+            //Время от отрисовки предыдущего кадра
+            miliseconds = 0;
+            //Все время
+            allTime = 0;
+            //Время требуемое для отрисовки кадра (время от отрисовки предыдущего кадра до следующего)
+            frameInMiliseconds = 1000 / (int)numericUpDown2.Value;
+            //Время полета меча от начала до нулевой высоты
+            timeVec = 0;
+            //Запускаем таймер
+            timer1.Start();
+        }
+
+        //Обработчик каждого тика таймера
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Увеличиваем время от отрисовки предыдущего кадра
+            miliseconds += timer1.Interval;
+            //Прибовляем ко всему времени работы модели
+            allTime += timer1.Interval;
+            //Увеличиваем время полета в текущем направлении
+            timeVec += timer1.Interval;
+            //Если пришло время отрисовывать следующий кадр
+            if (miliseconds >= frameInMiliseconds)
+            {
+                //Стираем все что было на панели
+                canvas.Graphics.Clear(Color.White);
+                //Рисуем график
+                DrawOXY();
+                //Получаем координаты и отрисовываем мяч
+                DrawBall(GetNextPoint());
+                //Выводим изображение
+                canvas.Render();
+                //Обнуляем время до следующего кадра
+                miliseconds = 0;
+                //Выводим время в текстбокс для времени
+                int mm = allTime / 60000 % 60;
+                int ss = allTime / 1000 % 60;
+                int fff = allTime % 1000;
+                textBox1.Text = mm.ToString("00") + ":" + ss.ToString("00") + ":" + fff.ToString("000");
+            }
+
+        }
+
+        //Нажатие на кнопку стоп
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            //Останавливаем таймер
+            timer1.Stop();
+        }
+        //Метод получения координат меча
+        private Point GetNextPoint()
+        {
+            //Текущее время
+            double t = timeVec / 1000.0;
+            //Текущая высота
+            double h = nH + nSpeed * t + (g * t * t) / 2;
+            //Текущая скорость
+            double speed = nSpeed + g * t;
+            //Если мяч достиг нулевой высоты, меняем направление меча
+            if (h <= 0)
+            {
+                //Теперь начальная высота ноль
+                nH = 0;
+                //А начальная скорость = прошлая скорость по модулю умноженая на упругость 
+                nSpeed = Math.Abs(speed) * upr;
+                //Обнуляем время полета до нулевой высоты
+                timeVec = 0;
+                //Обнуляем скорость и высоту для вывода на текстбоксы
+                speed = 0;
+                h = 0;
+            }
+            //Выводим скорость и высоту на текстбоксы
+            textBox7.Text = Math.Round(h).ToString();
+            textBox4.Text = Math.Round(speed).ToString();
+            //Вохвращаем точку отрисоки мяча
+            return GetPoint(ball_x, (int)(h * mInPix));
+        }
+        //Метод перевода координат в точку на панели
+        private Point GetPoint(int x, int y)
+        {
+            x += xcenter;
+            y += ycenter;
+            y = ycenter * 2 - y;
+            return new Point(x, y);
+        }
+        //Метод рисования меча
+        private void DrawBall(Point point)
+        {
+            brush.Color = Color.Red;
+            int R = 30;
+            canvas.Graphics.FillEllipse(brush, point.X - R, point.Y - R * 2, R * 2, R * 2);
+            canvas.Graphics.DrawEllipse(pen, point.X - R, point.Y - R * 2, R * 2, R * 2);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox2.Text, out double h))
+            {
+                nH = h;
+                canvas.Graphics.Clear(Color.White);
+                DrawOXY();
+                DrawBall(GetPoint(ball_x, (int)nH * mInPix));
+                canvas.Render();
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //Определяем писелей в метре
+            mInPix = (int)numericUpDown1.Value;
+            canvas.Graphics.Clear(Color.White);
+            DrawOXY();
+            DrawBall(GetPoint(ball_x, (int)nH * mInPix));
+            canvas.Render();
+        }
+    }
+}
